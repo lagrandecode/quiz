7784ad342f628bc842d9fe7ba908fe628c0cc07a
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2025-03-11 23:13:04 -0400

    Update settings.py

--- quiz/settings.py
+++ quiz/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+##%%
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
 #         'rest_framework.authentication.BasicAuthentication',
